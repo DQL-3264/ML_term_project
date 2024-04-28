4b85a28054eab8d05a00eaf081431f1c3d8d3f11
Codingfreshman <dequan0604@gmail.com>
2024-04-29 02:28:29 +0800

    Update model.py
    更新模型的架構

--- model.py
+++ model.py
@@ -1,5 +1,5 @@
 import numpy as np
-from utils import load_data,show_train_history
+from utils import load_data,show_train_history,show_prediction
 
 from sklearn.metrics import classification_report,accuracy_score
 
@@ -27,28 +27,34 @@ y_test_onehot=to_categorical(y_test,num_classes=2)
 # Model's architecture
 model=Sequential(name="CNN_ver1")
 
-model.add(Conv2D(20,7, activation="relu", padding="same" ,input_shape=(256,256,1)))
-model.add(MaxPooling2D(pool_size=(4,4),strides=2))
-model.add(BatchNormalization())
-model.add(Dropout(0.5))
-
-model.add(Conv2D(20,5, activation="relu", padding="same"))
+model.add(Conv2D(32,7, activation="relu", padding="same" ,input_shape=(128,128,1)))
 model.add(MaxPooling2D(pool_size=(2,2)))
 model.add(BatchNormalization())
-model.add(Dropout(0.5))
 
-model.add(Conv2D(40,5, activation="relu", padding="same"))
+model.add(Conv2D(32,5, activation="relu", padding="same"))
+model.add(MaxPooling2D(pool_size=(2,2)))
+model.add(Dropout(0.5))
+model.add(BatchNormalization())
+
+model.add(Conv2D(64,5, activation="relu", padding="same"))
 model.add(MaxPooling2D(pool_size=(2,2)))
 model.add(BatchNormalization())
-model.add(Dropout(0.5))
 
-model.add(Conv2D(40,3, activation="relu", padding="same"))
+model.add(Conv2D(64,5, activation="relu", padding="same"))
 model.add(MaxPooling2D(pool_size=(2,2)))
+model.add(Dropout(0.5))
 model.add(BatchNormalization())
-model.add(Dropout(0.5))
 
-model.add(Conv2D(80,3, activation="relu", padding="same"))
+model.add(Conv2D(64,3, activation="relu", padding="same"))
 model.add(MaxPooling2D(pool_size=(2,2)))
+model.add(Dropout(0.5))
+model.add(BatchNormalization())
+
+model.add(Conv2D(64,3, activation="relu", padding="same"))
+model.add(Dropout(0.5))
+model.add(BatchNormalization())
+
+model.add(Conv2D(128,3, activation="relu", padding="same"))
 
 model.add(Flatten())
 model.add(Dense(512, activation="relu"))
@@ -58,33 +64,39 @@ model.add(Dropout(0.5))
 model.add(Dense(2, activation="softmax"))
 print(model.summary())
 
-model.compile(loss='binary_crossentropy',metrics=['accuracy'],optimizer='rmsprop')
+model.compile(loss='binary_crossentropy',metrics=['accuracy'],optimizer='adam')
 
 # call back function
 model_checkpoint_callback =ModelCheckpoint(
-    filepath="Checkpoint/models.h5",
+    filepath="bestmodel.h5",
     monitor='val_accuracy',
     mode='max',
     save_best_only=True,
-    save_weights_only=True)
+    save_weights_only=True,
+    verbose=1
+    )
 
 # Train model
 history=model.fit(x_train_norm,y_train_onehot,
-                  epochs=30,
+                  epochs=50,
                   validation_split=0.2,
-                  batch_size=5)
-
-# Save final model
-model.save("final.h5")
+                  batch_size=10)
 
 # show the training history
 show_train_history(history)
 
-# Loading model
-model2=load_model("final.h5")
-
 # Predict testset
+model.load_weights("bestmodel.h5")
 pred=model.predict(x_test_norm)
 pred=np.argmax(pred,axis=1)
 print(classification_report(y_test, pred))
 print(accuracy_score(y_test, pred))
+
+# Predict ours pictures
+model.load_weights("bestmodel.h5")
+pred=np.load("photos.npy")
+pred.astype(np.float32)
+pred=pred/255
+res=model.predict(pred)
+res=np.argmax(res,axis=1)
+show_prediction(pred,res)
